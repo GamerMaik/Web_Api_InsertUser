ea520f449ef35e41af14c0e412a745c1d53b4226
ElTioCrack <felipezilian7@gmail.com>
2023-09-25 00:07:28 -0400

    new varible uwu

--- Api/Controllers/PersonController.cs
+++ Api/Controllers/PersonController.cs
@@ -10,7 +10,7 @@ namespace Api.Controllers
     {
         #region Atributos
         private LNProyectoApi lnProyectoApi;
-        private string UwU = "Uwuntu jajaj salu2 xdd";
+        private string UwU = "Uwuntu jajaj salu2 xddd";
  
         #endregion  
 
